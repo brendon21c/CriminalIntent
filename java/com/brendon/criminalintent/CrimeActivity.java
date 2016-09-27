eee8574db8f035478959cf81c3622d12ec75ab91
Brendon <brennon.mckeever@gmail.com>
2016-09-26 19:41:54 -0500

    CH 10 final

--- java/com/brendon/criminalintent/CrimeActivity.java
+++ java/com/brendon/criminalintent/CrimeActivity.java
@@ -1,17 +1,32 @@
 package com.brendon.criminalintent;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 
 public class CrimeActivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_CRIME_ID =
+            "com.brendon.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+
+    }
+
     @Override
     protected Fragment createFragment() {
 
-        return new CrimeFragment();
+        UUID crimeID = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeID);
 
     }
 }
--- java/com/brendon/criminalintent/CrimeListFragment.java
+++ java/com/brendon/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.brendon.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -36,27 +39,68 @@ public class CrimeListFragment extends Fragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
 
         CrimeLab crimeLab = CrimeLab.get(getActivity());
 
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+
+        } else {
+
+            mAdapter.notifyDataSetChanged();
+        }
+
+
 
     }
 
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public TextView mTitleTextView;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckbox;
+
+        private Crime mCrime;
+
 
         public CrimeHolder(View itemView) {
 
             super(itemView);
+            itemView.setOnClickListener(this);
 
-            mTitleTextView = (TextView) itemView;
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckbox = (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
+
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getID());
+            startActivity(intent);
+
+        }
+
+        public void bindCrime(Crime crime) {
+
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedCheckbox.setChecked(mCrime.isSolved());
 
         }
 
@@ -78,7 +122,7 @@ public class CrimeListFragment extends Fragment {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
 
             View view = layoutInflater
-                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+                    .inflate(R.layout.list_item_crime, parent, false);
 
             return new CrimeHolder(view);
 
@@ -88,7 +132,7 @@ public class CrimeListFragment extends Fragment {
         public void onBindViewHolder(CrimeHolder holder, int position) {
 
             Crime crime = mCrimes.get(position);
-            holder.mTitleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
 
 
